8c6846cea5d31e5f619241c051f7a3b9590a22c6
Steven Paalz <stevenpaalz@gmail.com>
2023-04-16 13:19:52 -0400

    add current user route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,8 +4,9 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
     message: "GET /api/users"
@@ -39,7 +40,7 @@ router.post("/register", async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       }
       catch(err) {
         next(err);
@@ -57,8 +58,22 @@ router.post('/login', async (req, res, next) => {
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user));
   })(req, res, next);
+});
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
