f7ae163234a9fa2d9cf7bb810ef7ff76fc9cac76
Steven Paalz <stevenpaalz@gmail.com>
2023-04-16 15:33:22 -0400

    create user validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,8 @@ const User = mongoose.model('User');
 const passport = require('passport');
 const { loginUser, restoreUser } = require('../../config/passport');
 const { isProduction } = require('../../config/keys');
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 router.get('/', function(req, res, next) {
   res.json({
@@ -13,7 +15,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post("/register", async (req, res, next) => {
+router.post("/register", validateRegisterInput, async (req, res, next) => {
   const user = await User.findOne({email: req.body.email});
 
   if (user) {
@@ -49,7 +51,7 @@ router.post("/register", async (req, res, next) => {
   });
 });
 
-router.post('/login', async (req, res, next) => {
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
---
+++ backend/validations/register.js
@@ -0,0 +1,22 @@
+const { check } = require("express-validator");
+const handleValidationErrors = require('./handleValidationErrors');
+
+const validateRegisterInput = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Email is invalid'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6, max: 30 })
+        .withMessage('Password must be between 6 and 30 characters'),
+    check('firstName')
+        .exists({checkFalsy: true})
+        .withMessage('First name is invalid'),
+    check('lastName')
+        .exists({checkFalsy: true})
+        .withMessage('Last name is invalid'),
+  handleValidationErrors
+];
+
+module.exports = validateRegisterInput;
