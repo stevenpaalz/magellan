792072db6aca0dea912b1886437d0784077cef33
Steven Paalz <stevenpaalz@gmail.com>
2023-04-16 15:40:53 -0400

    update seed file

--- seeders/seeds.js
+++ seeders/seeds.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const { mongoURI: db } = require('../config/keys.js');
 const User = require('../models/User');
-const Tweet = require('../models/Tweet');
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
 
