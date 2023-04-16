457938e09c7e90967800d8aac0f543d1ebc81d37
Steven Paalz <stevenpaalz@gmail.com>
2023-04-16 12:58:04 -0400

    add user login route

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const debug = require('debug');
 
-var usersRouter = require('./routes/api/users');
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
+const csurf = require('csurf');
+require('./models/User');
+require('./config/passport');
+const passport = require('passport');
+
+const usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf');
 
 var app = express();
 
@@ -11,6 +20,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(passport.initialize());
+
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);
+
 app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+  
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+});
 
 module.exports = app;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,11 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
     message: "GET /api/users"
   });
+});
+
+router.post("/register", async (req, res, next) => {
+  const user = await User.findOne({email: req.body.email});
+
+  if (user) {
+    const err = new Error("Validation Error");
+    err.statusCode = 400;
+    const errors = {};
+    if (user.email === req.body.email) {
+      errors.email = "A user has already registered with this email";
+    }
+    err.errors = errors;
+    return next(err);
+  }
+
+  const newUser = new User({
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+  });
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) throw err;
+    bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+      if (err) throw err;
+      try {
+        newUser.hashedPassword = hashedPassword;
+        const user = await newUser.save();
+        return res.json({ user });
+      }
+      catch(err) {
+        next(err);
+      }
+    })
+  });
+});
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', async function(err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error('Invalid credentials');
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json({ user });
+  })(req, res, next);
 });
 
 module.exports = router;
