ad16566e698ed5cf238d84c8f5ec489a2834fee4
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-04-17 15:29:37 -0400

    Homepage styling in progress

--- frontend/src/components/SplashPage/SplashPageText.js
+++ frontend/src/components/SplashPage/SplashPageText.js
@@ -1,8 +1,20 @@
 import React from "react";
+import "./SplashPage.css"
 
 const SplashPageText = () => {
     return (
-        <h1>This is the body text</h1>
+        <>
+        <div className="splash-page-body-text">
+            <div  className="splash-page-body-text">
+                The perfect way to explore a new city, or
+                fall in love all over again with the city you live in.
+                A great activity for friends, families, or work groups. 
+            </div>
+            <div  className="splash-page-body-text">
+                Today is the day to start questing!
+            </div>
+        </div>
+        </>
     )
 };
 
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -6,20 +6,20 @@ const SplashPage = () => {
     return (
       <>
         <div className="splash-page-body">
-        <div className="splash-left">
-          <div className="splash-left-hero-image-holder">
-            <div cassName="splash-page-hero-image"></div>
+          <div className="splash-left">
+            <div className="splash-left-hero-image-holder">
+              <img src="../../../assets/hero-image.png" alt="map-readers" className="splash-page-hero-image"/>
+            </div>
+          </div>        
+          <div className="splash-right">
+            <div className="splash-right-logo-holder">
+              <img src="../../../assets/logo.png" alt="Logo" className="splash-right-logo"/>
+            </div>
+            <div className="splash-right-text-holder">
+              <SplashPageText />
+            </div>
+            <div className="splash-right-cta"></div>
           </div>
-        </div>        
-        <div className="splash-right">
-          <div className="splash-right-logo-holder">
-            <div className="splash-right-logo"></div>
-          </div>
-          <div className="splash-right-text-holder">
-            <SplashPageText />
-          </div>
-          <div className="splash-right-cta"></div>
-        </div>
         </div>
       </>
     );
