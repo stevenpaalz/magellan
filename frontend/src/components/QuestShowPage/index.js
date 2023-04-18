edc89a5aaf805ee986774197368aa8164fb6ae15
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-04-18 16:57:19 -0400

    more styling more styling

--- frontend/src/components/QuestShowPage/index.js
+++ frontend/src/components/QuestShowPage/index.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getQuest } from "../../store/quests";
 import QuestShowTags from "./QuestShowTags";
+import QuestMap from "../Map";
 
 const QuestShowPage = () => {
 const { id } = useParams();
@@ -24,23 +25,25 @@ const { id } = useParams();
             <div className="quest-show-holder">
                 <div className="quest-show-left">
                     <div className="quest-show-map-holder">
-                        <div className="quest-show-map"></div>
                     </div>
+                    <div className="quest-show-reviews">Reviews</div>
                 </div>
                 <div className="quest-show-right">
-                    <div className="quest-show-header-holder">
+                    <div className="quest-show-header-holder" style={{ backgroundImage: `url(${quest.imageUrls[0]})`} }>
                             <div className="quest-show-title">{quest.title}</div>
-                            <img src={quest.imageUrls[0]} className="quest-show-image"/>                    </div>
-
-                    <div className="quest-show-tags-holder">
-                        <QuestShowTags tags={quest.tags} />
+                            // {/* <img src={quest.imageUrls[0]} className="quest-show-image"/>                     */}
                     </div>
+
+                    
 
                     <div className="quest-show-body-holder">
                         <div className="quest-show-description">{quest.description}</div>
-                        <div className="quest-show-radius">Radius: {quest.radius} miles</div>
-                        <div className="quest-show-time-needed">Duration: {quest.duration} hours</div>
-                        <div className="quest-show-reviews">Reviews Placeholder</div>
+                        <div className="quest-show-tags-holder">
+                        <QuestShowTags tags={quest.tags} />
+                    </div>
+                        
+                        <div className="quest-show-text">Radius: {quest.radius} miles</div>
+                        <div className="quest-show-text">Duration: {quest.duration} hours</div>
                     </div>
                     <div className="quest-show-buttons-holder">
                         <button className="quest-show-start-quest">Start Quest</button>
