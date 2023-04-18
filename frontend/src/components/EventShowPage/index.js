1dca636f1387308bcc489354cbc840f7ae8a9480
Steven Paalz <stevenpaalz@gmail.com>
2023-04-18 16:36:46 -0400

    events Show page skeleton

---
+++ frontend/src/components/EventShowPage/index.js
@@ -0,0 +1,27 @@
+import "./EventShowPage.css";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getEvent } from "../../store/events";
+
+function EventShowPage() {
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const event = useSelector(state => state.events[id]);
+
+    useEffect(() => {
+        dispatch(getEvent(id));
+    }, [dispatch, id]);
+
+    if (!event) return null;
+
+    return(
+        <>
+            <h1>hello from event show page {event._id}</h1>
+            <p>{event.quest.imageUrls}</p>
+        </>
+
+    )
+}
+
+export default EventShowPage
